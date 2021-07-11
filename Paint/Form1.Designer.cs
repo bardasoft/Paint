8ace17f4d37732cc3c7bff8cf223fad53d56b0dd
Mau Nguyen Van <nguyenvanmau9296@gmail.com>
2021-07-11 22:06:49 +0700

    Create Free line and Erase

--- Paint/Form1.Designer.cs
+++ Paint/Form1.Designer.cs
@@ -32,7 +32,7 @@ namespace Paint
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.btnLine = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnFree = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
@@ -40,8 +40,8 @@ namespace Paint
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnErase = new System.Windows.Forms.Button();
             this.picBoard = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
@@ -50,6 +50,7 @@ namespace Paint
             // 
             // tableLayoutPanel1
             // 
+            this.tableLayoutPanel1.AutoSize = true;
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
@@ -58,15 +59,15 @@ namespace Paint
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.11111F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.88889F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(804, 501);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.36727F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.63274F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(716, 501);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Controls.Add(this.btnLine);
-            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.btnFree);
             this.flowLayoutPanel1.Controls.Add(this.button3);
             this.flowLayoutPanel1.Controls.Add(this.button4);
             this.flowLayoutPanel1.Controls.Add(this.button5);
@@ -74,12 +75,12 @@ namespace Paint
             this.flowLayoutPanel1.Controls.Add(this.button7);
             this.flowLayoutPanel1.Controls.Add(this.button8);
             this.flowLayoutPanel1.Controls.Add(this.button9);
-            this.flowLayoutPanel1.Controls.Add(this.button10);
-            this.flowLayoutPanel1.Controls.Add(this.button11);
+            this.flowLayoutPanel1.Controls.Add(this.btnClear);
+            this.flowLayoutPanel1.Controls.Add(this.btnErase);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(798, 79);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(710, 76);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // btnLine
@@ -90,119 +91,128 @@ namespace Paint
             this.btnLine.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLine.Location = new System.Drawing.Point(3, 3);
             this.btnLine.Name = "btnLine";
-            this.btnLine.Size = new System.Drawing.Size(64, 64);
+            this.btnLine.Size = new System.Drawing.Size(48, 48);
             this.btnLine.TabIndex = 0;
             this.btnLine.Text = "Line";
             this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
             // 
-            // button2
+            // btnFree
             // 
-            this.button2.FlatAppearance.CheckedBackColor = System.Drawing.Color.Teal;
-            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
-            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(73, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(64, 64);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "button1";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnFree.FlatAppearance.CheckedBackColor = System.Drawing.Color.Teal;
+            this.btnFree.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnFree.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.btnFree.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFree.Location = new System.Drawing.Point(57, 3);
+            this.btnFree.Name = "btnFree";
+            this.btnFree.Size = new System.Drawing.Size(48, 48);
+            this.btnFree.TabIndex = 0;
+            this.btnFree.Text = "Free";
+            this.btnFree.UseVisualStyleBackColor = true;
+            this.btnFree.Click += new System.EventHandler(this.btnFree_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(143, 3);
+            this.button3.Location = new System.Drawing.Point(111, 3);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(64, 64);
+            this.button3.Size = new System.Drawing.Size(48, 48);
             this.button3.TabIndex = 0;
             this.button3.Text = "button1";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(213, 3);
+            this.button4.Location = new System.Drawing.Point(165, 3);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(64, 64);
+            this.button4.Size = new System.Drawing.Size(48, 48);
             this.button4.TabIndex = 0;
             this.button4.Text = "button1";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(283, 3);
+            this.button5.Location = new System.Drawing.Point(219, 3);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(64, 64);
+            this.button5.Size = new System.Drawing.Size(48, 48);
             this.button5.TabIndex = 0;
             this.button5.Text = "button1";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(353, 3);
+            this.button6.Location = new System.Drawing.Point(273, 3);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(64, 64);
+            this.button6.Size = new System.Drawing.Size(48, 48);
             this.button6.TabIndex = 0;
             this.button6.Text = "button1";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(423, 3);
+            this.button7.Location = new System.Drawing.Point(327, 3);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(64, 64);
+            this.button7.Size = new System.Drawing.Size(48, 48);
             this.button7.TabIndex = 0;
             this.button7.Text = "button1";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(493, 3);
+            this.button8.Location = new System.Drawing.Point(381, 3);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(64, 64);
+            this.button8.Size = new System.Drawing.Size(48, 48);
             this.button8.TabIndex = 0;
             this.button8.Text = "button1";
             this.button8.UseVisualStyleBackColor = true;
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(563, 3);
+            this.button9.Location = new System.Drawing.Point(435, 3);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(64, 64);
+            this.button9.Size = new System.Drawing.Size(48, 48);
             this.button9.TabIndex = 0;
             this.button9.Text = "button1";
             this.button9.UseVisualStyleBackColor = true;
             // 
-            // button10
+            // btnClear
             // 
-            this.button10.Location = new System.Drawing.Point(633, 3);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(64, 64);
-            this.button10.TabIndex = 0;
-            this.button10.Text = "button1";
-            this.button10.UseVisualStyleBackColor = true;
+            this.btnClear.Location = new System.Drawing.Point(489, 3);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(48, 48);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
-            // button11
+            // btnErase
             // 
-            this.button11.Location = new System.Drawing.Point(703, 3);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(64, 64);
-            this.button11.TabIndex = 0;
-            this.button11.Text = "button1";
-            this.button11.UseVisualStyleBackColor = true;
+            this.btnErase.Location = new System.Drawing.Point(543, 3);
+            this.btnErase.Name = "btnErase";
+            this.btnErase.Size = new System.Drawing.Size(48, 48);
+            this.btnErase.TabIndex = 0;
+            this.btnErase.Text = "Erase";
+            this.btnErase.UseVisualStyleBackColor = true;
+            this.btnErase.Click += new System.EventHandler(this.btnErase_Click);
             // 
             // picBoard
             // 
             this.picBoard.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.picBoard.Location = new System.Drawing.Point(3, 88);
+            this.picBoard.Location = new System.Drawing.Point(3, 85);
             this.picBoard.Name = "picBoard";
-            this.picBoard.Size = new System.Drawing.Size(798, 410);
+            this.picBoard.Size = new System.Drawing.Size(710, 413);
             this.picBoard.TabIndex = 1;
             this.picBoard.TabStop = false;
+            this.picBoard.SizeChanged += new System.EventHandler(this.picBoard_SizeChanged);
+            this.picBoard.Paint += new System.Windows.Forms.PaintEventHandler(this.picBoard_Paint);
+            this.picBoard.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picBoard_MouseDown);
+            this.picBoard.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picBoard_MouseMove);
+            this.picBoard.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picBoard_MouseUp);
             // 
             // frmPaint
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(804, 501);
+            this.ClientSize = new System.Drawing.Size(716, 501);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "frmPaint";
             this.Text = "Paint";
@@ -210,6 +220,7 @@ namespace Paint
             this.flowLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.picBoard)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -218,7 +229,7 @@ namespace Paint
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button btnLine;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnFree;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
@@ -226,8 +237,8 @@ namespace Paint
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnErase;
         private System.Windows.Forms.PictureBox picBoard;
     }
 }
--- Paint/Form1.cs
+++ Paint/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,10 @@ using System.Windows.Forms;
 
 namespace Paint
 {
-    enum Shapes
+    internal enum Shapes
     {
         None,
+        Erase,
         Free,
         Line,
         Rectangle,
@@ -26,10 +28,132 @@ namespace Paint
     }
     public partial class frmPaint : Form
     {
+        bool paint;
+        private Graphics graphic;
+        Bitmap bitmap;
+        private bool filled;
+        Point startPoint;
+        Point endPoint;
+        private Pen pen;
+        float widthPen =10;
+        private Pen erase;
+        float widthErase = 20;
+        private Brush brush;
+        private Shapes shape;
+
         public frmPaint()
         {
             InitializeComponent();
+            paint = false;
+            bitmap = new Bitmap(picBoard.Width, picBoard.Height);
+            graphic = Graphics.FromImage(bitmap);
+            graphic.SmoothingMode = SmoothingMode.HighQuality;
+            graphic.Clear(Color.White);
+            picBoard.Image = bitmap;
+
+            pen = new Pen(Color.Black,widthPen);
+            erase = new Pen(Color.White, widthErase);
+
+
+        }
+        private void picBoard_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+            startPoint = e.Location;
         }
 
+        private void picBoard_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (paint)
+            {
+                if (shape == Shapes.Free)
+                {
+                    endPoint = e.Location;
+                    graphic.DrawLine(pen, startPoint, endPoint);
+                    startPoint = endPoint;
+                }
+                else if(shape == Shapes.Erase)
+                {
+
+                    endPoint = e.Location;
+                    graphic.DrawLine(erase, startPoint, endPoint);
+                    startPoint = endPoint;
+                }
+
+
+            }
+
+            picBoard.Refresh();
+        }
+
+        private void picBoard_MouseUp(object sender, MouseEventArgs e)
+        {
+            // picBoard.Refresh();
+            paint = false;
+
+        }
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            shape = Shapes.Line;
+        }
+
+        private void btnFree_Click(object sender, EventArgs e)
+        {
+            shape = Shapes.Free;
+        }
+
+        private void picBoard_Paint(object sender, PaintEventArgs e)
+        {
+            switch (shape)
+            {
+                case Shapes.None:
+                    break;
+                case Shapes.Free:
+                    break;
+                case Shapes.Line:
+                    graphic.DrawLine(pen, startPoint, endPoint);
+                    break;
+                case Shapes.Rectangle:
+                    break;
+                case Shapes.Square:
+                    break;
+                case Shapes.Elip:
+                    break;
+                case Shapes.Circle:
+                    break;
+                case Shapes.Triangle:
+                    break;
+                case Shapes.Petagon:
+                    break;
+                case Shapes.Hecxagon:
+                    break;
+                case Shapes.Fill:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+
+        private void picBoard_SizeChanged(object sender, EventArgs e)
+        {
+            bitmap = new Bitmap(picBoard.Width, picBoard.Height);
+            graphic = Graphics.FromImage(bitmap);
+            graphic.Clear(Color.White);
+            picBoard.Image = bitmap;
+        }
+
+        private void btnErase_Click(object sender, EventArgs e)
+        {
+            shape = Shapes.Erase;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            graphic.Clear(Color.White);
+            picBoard.Refresh();
+        }
     }
 }
